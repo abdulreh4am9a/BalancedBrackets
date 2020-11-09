c06b1b22cd961f5708778f54899013daac50fc38
abdulreh4am9a <abdulreh4am9a@gmail.com>
2020-11-09 23:40:58 +0500

    Add project files.

---
+++ 04071913025_AbdulRehman_A3(1)/Part1.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+template<typename T>
+struct node {
+	T data;
+	node<T>* next;
+};
+template<typename T>
+class stack {
+	node<T>* top_ptr;
+public:
+	stack() {
+		top_ptr = NULL;
+	}
+	~stack() {
+		make_empty();
+	}
+	void push(const T& val) {
+		node<T>* temp;
+		temp = new node<T>;
+		temp->data = val;
+		temp->next = top_ptr;
+		top_ptr = temp;
+	}
+	void pop() {
+		node<T>* temp;
+		temp = top_ptr;
+		top_ptr = top_ptr->next;
+		delete temp;
+	}
+	T top() const {
+		return top_ptr->data;
+	}
+	bool empty() const {
+		return top_ptr == NULL;
+	}
+	bool full() const {
+		node<T>* temp;
+		temp = new node<T>;
+		if (temp == NULL)
+			return true;
+		else {
+			delete temp;
+			return false;
+		}
+	}
+	void make_empty() {
+		while (!empty()) {
+			pop();
+		}
+	}
+};
+bool balanceCheck(const char* filename) {
+	char c;
+	stack<char> s;
+	ifstream thefile(filename);
+	if (thefile.is_open()) {
+		while (thefile.good()) {
+			thefile.get(c);
+			if (thefile.gcount() == 0)
+				break;
+			if (c == '(' || c == '{' || c == '[')
+				s.push(c);
+			else if (c == ')') {
+				if (s.top() == '(')
+					s.pop();
+				else
+					return false;
+			}
+			else if (c == '}') {
+				if (s.top() == '{')
+					s.pop();
+				else
+					return false;
+			}
+			else if (c == ']') {
+				if (s.top() == '[')
+					s.pop();
+				else
+					return false;
+			}
+		}
+		return s.empty();
+	}
+	else {
+		cout << "File doesn't exist. Please create a file!" << endl;
+		//return false;
+		exit(1);
+	}
+}
+int main(int argc, char* argv[]) {
+	if (balanceCheck(argv[1]))
+		cout << "Balanced Equation" << endl;
+	else
+		cout << "Not Balanced" << endl;
+	return 0;
+}
