1b8f963a596450760984136757952ef81b28c7ca
abdulreh4am9a <abdulreh4am9a@gmail.com>
2020-11-10 21:12:49 +0500

    Modified

--- 04071913025_AbdulRehman_A3(1)/Part1.cpp
+++ 04071913025_AbdulRehman_A3(1)/Part1.cpp
@@ -18,6 +18,8 @@ public:
 		make_empty();
 	}
 	void push(const T& val) {
+		if (full())
+			throw("Stack Overflow!");
 		node<T>* temp;
 		temp = new node<T>;
 		temp->data = val;
@@ -25,12 +27,16 @@ public:
 		top_ptr = temp;
 	}
 	void pop() {
+		if (empty())
+			throw("Stack Underflow!");
 		node<T>* temp;
 		temp = top_ptr;
 		top_ptr = top_ptr->next;
 		delete temp;
 	}
 	T top() const {
+		if (empty())
+			throw("Stack is empty!");
 		return top_ptr->data;
 	}
 	bool empty() const {
