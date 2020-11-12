d487ba261d12f1fc095d1772bd357b6fc08b4281
abdulreh4am9a <abdulreh4am9a@gmail.com>
2020-11-12 14:38:26 +0500

    Final

--- 04071913025_AbdulRehman_A3(1)/Part1.cpp
+++ 04071913025_AbdulRehman_A3(1)/Part1.cpp
@@ -1,17 +1,18 @@
 #include"stack.h"
 #include<fstream>
 #include<string>
+//function to check the brackets in the file are balanced or not
 bool balanceCheck(const char* filename) {
-	char c;
-	stack<char> s;
-	ifstream thefile(filename);
+	char c; //character to read file character by character
+	stack<char> s; //stack to push or pop the brackets when appeared
+	ifstream thefile(filename); //opening the file in read mode
 	if (thefile.is_open()) {
-		while (thefile.good()) {
-			thefile.get(c);
-			if (thefile.gcount() > 0) {
-				if (c == '(' || c == '{' || c == '[')
+		while (thefile.good()) { //while the file is not ended
+			thefile.get(c); //getting a char from file
+			if (thefile.gcount() > 0) { //check to ignore double consideration of last character
+				if (c == '(' || c == '{' || c == '[') //if opening bracket appears push it to stack
 					s.push(c);
-				else if (c == ')') {
+				else if (c == ')') { //else if any closing bracket appears then the top most element at the stack must be its opening bracket otherwise the brackets are not  balance
 					if (s.top() == '(')
 						s.pop();
 					else
@@ -31,7 +32,7 @@ bool balanceCheck(const char* filename) {
 				}
 			}
 		}
-		return s.empty();
+		return s.empty(); //in the end if the stack is empty then it means there is no bracket without pair so the eqaution is balanced otherwise it is not balanced
 	}
 	else {
 		cout << "File doesn't exist. Please create a file!" << endl;
